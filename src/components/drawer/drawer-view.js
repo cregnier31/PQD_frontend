915266af6fdc0cd6af3b506cc834ffe66c790e95
Yazid Hassani <yha@ffly4u.com>
2019-12-24 18:51:51 +0100

    [LAYOUT] - Add component Drawer

---
+++ src/components/drawer/drawer-view.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import {Filter} from '../filter';
+
+
+const drawerWidth = 200;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  toolbar: theme.mixins.toolbar,
+  region: {
+    justifyContent: 'space-between',
+  }
+}));
+
+export function DrawerView() {
+
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.toolbar} />
+        <List>
+          {['BLUE', 'GREEN', 'WHITE'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          <div className={'container-filters'}>
+            <Filter />
+          </div>
+        </List>
+      </Drawer>
+    </div>
+  );
+}
