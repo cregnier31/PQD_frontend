2e21a53b36750edb4cc5056e10ad280fee23714d
Yazid Hassani <yha@ffly4u.com>
2019-12-26 15:29:07 +0100

    [LAYOUT] - Modification component "app", "content" and "header" (drawer, page responsive...)

--- src/components/content/content-view.js
+++ src/components/content/content-view.js
@@ -2,13 +2,16 @@ import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
+import Divider from '@material-ui/core/Divider';
+import Crop169Icon from '@material-ui/icons/Crop169';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import WebIcon from '@material-ui/icons/Web';
 import {Widget} from '../widget1';
 import {Panel} from '../panel';
-import {Drawer} from '../drawer';
 
 const useStyles = makeStyles(theme => ({
   card: {
-    backgroundColor: '#92AFCD',
+    backgroundColor: '#92AFCD', // Props color
   },
 }));
 
@@ -23,7 +26,17 @@ export function ContentView(props){
       </Grid>
 
       <Card className={classes.card}>
-        <h3>BLUE</h3>
+        <Grid container direction="row" alignItems="center">
+          <Grid item xs={1} md={1}>
+            <Crop169Icon fontSize="large" style={{ color: 'blue'}} />
+            {/* Props color */}
+          </Grid>
+          <Grid item xs={1} md={1}>
+            <h3>Blue</h3>
+            {/* Props color */}
+          </Grid>
+        </Grid>
+
         <Grid container>
           <Grid item xs={12} md={8}>
             <Widget/>
@@ -31,6 +44,25 @@ export function ContentView(props){
           <Grid item xs={12} md={4}>
             <Widget/>
           </Grid>
+        </Grid>
+
+        <Divider />
+
+        <Grid container direction="row" alignItems="flex-start">
+          <Grid item xs={4} md={4}>
+            Sous titre
+          </Grid>
+          <Grid item xs={4} md={4}>
+            <MenuBookIcon />
+            Catalogue
+          </Grid>
+          <Grid item xs={4} md={4}>
+            <WebIcon />
+            Site producteur
+          </Grid>
+        </Grid>
+
+        <Grid container>
           <Grid item xs={12} md={4}>
             <Widget/>
           </Grid>
@@ -42,7 +74,6 @@ export function ContentView(props){
           </Grid>
         </Grid>
       </Card>
-      <Drawer />
     </div>
   );
 }
--- src/components/header/header-view.js
+++ src/components/header/header-view.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import FilterListIcon from '@material-ui/icons/FilterList';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import {Filter} from '../filter';
+import {Content} from '../content';
 
-
-const drawerWidth = 200;
+const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -26,19 +37,43 @@ const useStyles = makeStyles(theme => ({
     width: drawerWidth,
   },
   toolbar: theme.mixins.toolbar,
-  region: {
-    justifyContent: 'space-between',
-  }
+  menuButton: {
+    float: 'left',
+  },
+  hide: {
+    display: 'none',
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
 }));
 
 export function HeaderView() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(true);
   const [value, setValue] = React.useState(0);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  const classes = useStyles();
   const regions = [
     'GLOBAL',
     'ARTIC',
@@ -48,10 +83,29 @@ export function HeaderView() {
     'NORTH WESTERN SHELVES',
     'MEDITERRANEAN SEA'
   ];
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className={classes.root}>
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar className={classes.region}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
           <ArrowBackIcon />
           <Tabs
             value={value}
@@ -64,6 +118,50 @@ export function HeaderView() {
           </Tabs>
         </Toolbar>
       </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.toolbar} />
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <List>
+          {['BLUE', 'GREEN', 'WHITE'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          <div className={'container-filters'}>
+            <Filter />
+          </div>
+        </List>
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+        <IconButton
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <FilterListIcon />
+          </IconButton>
+        <Content />
+      </main>
     </div>
   );
 }
