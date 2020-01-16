f12a7d8e24edfdb4f2a856008faf07bf5038cfe8
julien <julien.schiele@gmail.com>
2020-01-16 15:46:16 +0100

    clean

--- src/components/universePanel/widgetMap/widgetMap-view.js
+++ src/components/universePanel/widgetMap/widgetMap-view.js
@@ -1,23 +1,7 @@
 import React from "react";
 import Card from "./widgetMap-styles";
 import LeafletMap from "./leafletMap";
-import { makeStyles } from '@material-ui/core/styles';
 import {Widget} from './../widget';
-
-const useStyles = makeStyles(theme => ({
-  right: {
-    float: 'right',
-  },
-  left: {
-    float: 'left',
-    marginLeft: '10px'
-  },
-  content: {
-    height: '100% !important',
-    marginTop: '0px !important',
-    marginBottom: '0px !important',
-  },
-}));
 
 export function WidgetMapView(props){
   return (
